3178e1c1ea6095fca8db814aad35cb45d4becf2e
rom <romeojoseph56@gmail.com>
2023-10-16 12:14:30 +0530

    var file

--- aws_ec2/ec2.tf
+++ aws_ec2/ec2.tf
@@ -1,11 +1,5 @@
 resource "aws_instance" "myinstance" {
-  ami           = "ami-03d294e37a4820c21"
-  instance_type = "t2.small"
-  count         = 1
-}
-resource "aws_s3_bucket" "mybucket" {
-  bucket = "my-tfet0000000000"
-}
-resource "aws_vpc" "myvpc0022" {
-  cidr_block       = "10.0.0.0/16"
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  count         = var.instance_count
 }
